3e7f88e3e8040a8dfed614155362dc97ff7a2122
Indra Hasan <indra.hasan.072020@gmail.com>
2021-12-19 13:38:03 +0700

    add health check

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { databaseProvider } from "../providers/database.provider";
+import { TerminusModule } from "@nestjs/terminus";
+import { HealthController } from "./health/controllers/health.controller";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TerminusModule, HttpModule],
+  controllers: [HealthController],
   providers: [...databaseProvider],
-  exports:[...databaseProvider],
+  exports: [...databaseProvider]
 })
-export class ApiModule {}
+export class ApiModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from "./api/api.module";
+import { Config } from './helpers/config.helper';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
-  await app.listen(3000);
+  await app.listen(Config.getNumber('APP_PORT'));
 }
 bootstrap();
